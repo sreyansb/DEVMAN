78a157fcf2b9eed5d1133f7479f7b4c91e52bf43
sreyansb <43511601+sreyansb@users.noreply.github.com>
2019-08-06 15:20:30 +0530

    Delete devpassmake.mk

--- devpassmake.mk
+++
@@ -1,8 +0,0 @@
-devpassk.out : devpass.o ui.o rsa.o 
-	gcc devpass.o ui.o rsa.o -o devpassk.out -lm
-devpass.o : devpass.c 
-	gcc -c devpass.c -lm
-ui.o : ui.c
-	gcc -c ui.c -lm
-rsa.o : rsa.c
-	gcc rsa.c -c -lm
