cf0ca47e569e4e6955ca8a894a4ca3b114c686bc
sreyansb <43511601+sreyansb@users.noreply.github.com>
2019-08-06 15:20:49 +0530

    Delete seepassmake.mk

--- seepassmake.mk
+++
@@ -1,8 +0,0 @@
-seepassk.out : seepass.o ui.o rsa.o
-	gcc seepass.o ui.o rsa.o -o seepassk.out -lm
-seepass.o : seepass.c 
-	gcc -c seepass.c -lm
-ui.o : ui.c
-	gcc -c ui.c -lm
-rsa.o : rsa.c
-	gcc rsa.c -c -lm
